3c8d0bebc1a7da5e7c75211714a5c9518ba26dba
shijiaqi <2280191633@qq.com>
2020-03-25 08:23:28 +0000

    mongodb

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Facade\FlareClient\Http\Client;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\DB;
+use MongoDB\Client as MongoDB;
 
 class TestController extends Controller
 {
-    //
+    //REDIS
     public function redis1(){
         $key = 'aaa';
         $val = 'hello';
@@ -25,5 +27,28 @@ class TestController extends Controller
     public function mysql1(){
         $list = DB::table('register')->first();
         var_dump($list);
+    }
+    //
+    public function mysql2(){
+        $data =[
+            'tel'=> '11111111111',
+            'email'=>'2280191635@qq.com',
+        ];
+        $list = DB::table('register')->insert($data);
+        var_dump($list);
+    }
+
+    //
+    public function mongo1(){
+        $host = "192.168.62.190";
+        $db = "ln1906";
+        $tab = "users";
+        echo __METHOD__;echo"<br>";
+        $client = new Client("mongodb://{$host}:27017");
+
+        $collection = $client->$db->$tab;
+        $result = $collection->find()->toArray();
+        echo "<pre>";print_r($result);echo "</pre>";
+        var_dump($result);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,15 @@ Route::any('/redis1','TestController@redis1');
 Route::any('/redis2','TestController@redis2');
 //mysql
 Route::any('/mysql1','TestController@mysql1');
+Route::any('/mysql2','TestController@mysql2');
 
+//mongodb
+Route::any('/mongo1','TestController@mongo1');
+
+
+
+
+###################################################
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
